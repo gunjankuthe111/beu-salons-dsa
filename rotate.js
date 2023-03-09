0dbb530cfc1979bb171f1e1d287a7e4734d4dd7a
Gunjan Kuthe <gkuthe1997@gmail.com>
2023-03-09 12:23:11 +0530

    added N rotation question

--- rotate.js
+++ rotate.js
@@ -1,3 +1,15 @@
+// You are given an array of length N (1 < N <= 100) and a number K. Print the array after K rotations in clockwise direction.
+// E.g. 
+// N = 10, K = 3
+// Arr = [1,2,3,4,5,6,7,8,9,10]
+// after K rotations
+// Arr = [4,5,6,7,8,9,10,1,2,3]
+
+// NOTE:
+// Please mention use of any in-built javascript function is not allowed like shift, unshift, substr etc.
+// You can use any editor of your choice.
+
+
 let N = 10;
 let K = 3;
 let arr = [1,2,3,4,5,6,7,8,9,10];
